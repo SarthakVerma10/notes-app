0c9c2d6208b684b4b48d85e048edb8afec184dc6
SarthakVerma10 <70906968+SarthakVerma10@users.noreply.github.com>
2021-02-21 15:52:01 +0530

    Delete src/playground directory

--- src/playground/reduce.js
+++
@@ -1,8 +0,0 @@
-const data = [1,2,3,4]
-
-const reducer = (acc, currVal) => {
-  console.log(` acc = ${acc} and currVal = ${currVal}`);
-  return acc + currVal;
-}
-
-console.log(data.reduce(reducer));
